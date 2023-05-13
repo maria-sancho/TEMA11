89c2a2a4751a2938671859e73df16c71f4de592d
maria-sancho <alusan7934@ieselcaminas.org>
2023-05-13 16:29:13 +0200

    TEMA11

--- src/OrdenaNombres.java
+++ src/OrdenaNombres.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class OrdenaNombres {
+import java.io.*;
+import java.util.*;
+
+public class OrdenaNombres {
+    public static void main(String[] args) {
+        List<String> nombres = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            FileInputStream fis = new FileInputStream("nombres.txt");
+            Scanner sc = new Scanner(fis);
+
+            while (sc.hasNextLine()) {
+                nombres.add(sc.nextLine());
+            }
+
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String nombre;
+        do {
+            System.out.print("Introduzca un nombre (o 'fin' para terminar): ");
+            nombre = scanner.nextLine();
+
+            if (!nombre.equalsIgnoreCase("fin")) {
+                int indice = 0;
+                while (indice < nombres.size() && nombres.get(indice).compareToIgnoreCase(nombre) < 0) {
+                    indice++;
+                }
+                nombres.add(indice, nombre);
+            }
+        } while (!nombre.equalsIgnoreCase("fin"));
+
+        scanner.close();
+
+        try {
+            FileOutputStream fos = new FileOutputStream("nombres.txt");
+            PrintWriter pw = new PrintWriter(fos);
+
+            for (String n : nombres) {
+                pw.println(n);
+            }
+
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/OrdenaNumeros.java
+++ src/OrdenaNumeros.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class OrdenaNumeros {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class OrdenaNumeros {
+
+    public static void main(String[] args) {
+        String archivoEntrada = "numeros.dat";
+        String archivoPares = "pares.dat";
+        String archivoImpares = "impares.dat";
+        try (DataInputStream lector = new DataInputStream(new FileInputStream(archivoEntrada));
+             DataOutputStream escritorPares = new DataOutputStream(new FileOutputStream(archivoPares));
+             DataOutputStream escritorImpares = new DataOutputStream(new FileOutputStream(archivoImpares))) {
+            while (lector.available() > 0) {
+                int numero = lector.readInt();
+                if (numero % 2 == 0) {
+                    escritorPares.writeInt(numero);
+                } else {
+                    escritorImpares.writeInt(numero);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer o escribir en archivo.");
+        }
+    }
 }
